50358de7bdf66d6d7ee5518bf09196abd68640e8
roneydev01 <roneyfreitas.rf@gmail.com>
2021-06-15 22:40:46 -0300

    style: improved the style

--- src/ui/components/feedback/SafeEnveroment/SafeEnviroment.style.tsx
+++ src/ui/components/feedback/SafeEnveroment/SafeEnviroment.style.tsx
@@ -1,7 +1,10 @@
 import { styled } from '@material-ui/core/styles';
 
+//Utilizando rrecurso do Material-UI Theme
 export const SafeEnviromentContainer = styled('div')`
   text-align: right;
-  padding: 16px 0;
   font-size: 12px;
-`
+  color: ${({ theme }) => theme.palette.text.secondary};
+  background-color: ${({ theme }) => theme.palette.background.default};
+  padding: ${({ theme }) => theme.spacing(2)} 0;
+`;
